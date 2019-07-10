3bd96f3502d63b53e73060f4b947d3de3fe7e96d
abhikedia <36509361+abhikedia@users.noreply.github.com>
2019-07-10 16:06:58 +0530

    Update ExampleToken.sol

--- ExampleToken.sol
+++ ExampleToken.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.5.0;
 import './StandardToken.sol';
 
 contract ExampleToken is StandardToken {
-  string public name = "BlockHire";
-  string public symbol = "BH";
+  string public name = "Geth-Hired";
+  string public symbol = "GH";
   uint public decimals = 18;
   uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
   //uint256 public totalTokens;
